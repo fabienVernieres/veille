7c66093a118cbd2036f426e9311a514a5eb5a605
fabien Vernières <fabienvernieres@gmail.com>
2023-03-03 21:26:11 +0100

    Thème boostrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use App\Repository\FlowRepository;
 use Laminas\Feed\Reader\Reader;
+use App\Repository\FlowRepository;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
@@ -13,32 +15,51 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(FlowRepository $flowRepository): Response
     {
+        /** @var array Tableau regroupant les données des différents flux. */
         $arrayData = [];
-        $flows = $flowRepository->findBy(['User' => $this->getUser()]);
 
-        foreach ($flows as $flow) {
-            $feed = Reader::import($flow->getUrl());
-            $data = [
-                'title'        => $feed->getTitle(),
-                'link'         => $feed->getLink(),
-                'dateModified' => $feed->getDateModified(),
-                'description'  => $feed->getDescription(),
-                'language'     => $feed->getLanguage(),
-                'entries'      => [],
-            ];
+        if ($this->getUser()) {
+            /** Définition du nom du cache. */
+            $cache = new FilesystemAdapter();
+            $cacheName = 'cache-' . $this->getUser()->getUserIdentifier();
+            $cacheName = str_replace('@', '', $cacheName);
 
-            foreach ($feed as $entry) {
-                $edata = [
-                    'title'        => $entry->getTitle(),
-                    'description'  => $entry->getDescription(),
-                    'dateModified' => $entry->getDateModified(),
-                    'authors'      => $entry->getAuthors(),
-                    'link'         => $entry->getLink(),
-                    'content'      => $entry->getContent(),
-                ];
-                $data['entries'][] = $edata;
+            /** @var array Liste des flux de l'utilisateur. */
+            $flows = $flowRepository->findBy(['User' => $this->getUser()]);
+
+            if ($flows) {
+                /** Pour chaque flux les données sont stockées dans $arrayData. */
+                $arrayData = $cache->get($cacheName, function (ItemInterface $item) use ($flows) {
+                    $item->expiresAfter(3600);
+
+                    foreach ($flows as $flow) {
+                        $feed = Reader::import($flow->getUrl());
+                        $data = [
+                            'title'        => $feed->getTitle(),
+                            'link'         => $feed->getLink(),
+                            'dateModified' => $feed->getDateModified(),
+                            'description'  => $feed->getDescription(),
+                            'language'     => $feed->getLanguage(),
+                            'entries'      => [],
+                        ];
+
+                        foreach ($feed as $entry) {
+                            $edata = [
+                                'title'        => $entry->getTitle(),
+                                'description'  => $entry->getDescription(),
+                                'dateModified' => $entry->getDateModified(),
+                                'authors'      => $entry->getAuthors(),
+                                'link'         => $entry->getLink(),
+                                'content'      => $entry->getContent(),
+                            ];
+                            $data['entries'][] = $edata;
+                        }
+                        $arrayData[] = $data;
+                    }
+
+                    return $arrayData;
+                });
             }
-            $arrayData[] = $data;
         }
 
         return $this->render('home/index.html.twig', [
---
+++ src/Security/Voter/FlowVoter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class FlowVoter extends Voter
+{
+    public const EDIT = 'POST_EDIT';
+    public const VIEW = 'POST_VIEW';
+    public const DELETE = 'POST_DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::VIEW, self::DELETE])
+            && $subject instanceof \App\Entity\Flow;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::EDIT:
+                return $user == $subject->getUser();
+                break;
+            case self::VIEW:
+                return $user == $subject->getUser();
+                break;
+            case self::DELETE:
+                return $user == $subject->getUser();
+                break;
+        }
+
+        return false;
+    }
+}
