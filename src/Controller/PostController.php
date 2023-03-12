27d78b170aedc9946ecf24ed2a74290c2f841a5a
fabien Vernières <fabienvernieres@gmail.com>
2023-03-12 17:23:52 +0100

    correction template h1 et padding

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -17,11 +17,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PostController extends AbstractController
 {
     #[Route('/new', name: 'app_post_new', methods: ['POST'])]
+    /**
+     * Ajoute un nouveau post aux favoris.
+     *
+     * @param  EntityManagerInterface $entityManager
+     * @param  PostRepository $postRepository
+     * @return JsonResponse
+     */
     public function new(EntityManagerInterface $entityManager, PostRepository $postRepository): JsonResponse
     {
-        // Create a new user object
+        // Crée un nouvel objet Post.
         $post = new Post();
 
+        // Récupère le body de la requête et le convertit en tableau.
         $request = new Request($_POST);
         $request = $request->toArray();
 
@@ -32,6 +40,7 @@ class PostController extends AbstractController
         $post->setCreatedAt(new DateTimeImmutable());
 
         try {
+            // Si le post n'est pas déjà sauvegardé, enregistre le en favori.
             if (!$postRepository->findBy(['user' => $post->getUser(), 'link' => $post->getLink()])) {
                 $entityManager->persist($post);
                 $entityManager->flush();
@@ -43,6 +52,12 @@ class PostController extends AbstractController
     }
 
     #[Route('/', name: 'app_post', methods: ['GET'])]
+    /**
+     * Affiche les favoris de l'utilisateur.
+     *
+     * @param  PostRepository $postRepository
+     * @return Response
+     */
     public function index(PostRepository $postRepository): Response
     {
         $posts = $postRepository->findBy(['user' => $this->getUser()], ['id' => 'DESC']);
@@ -52,11 +67,19 @@ class PostController extends AbstractController
     }
 
     #[Route('/delete', name: 'app_post_delete', methods: ['POST'])]
+    /**
+     * Supprime un post des favoris.
+     *
+     * @param  EntityManagerInterface $entityManager
+     * @param  PostRepository $postRepository
+     * @return JsonResponse
+     */
     public function delete(EntityManagerInterface $entityManager, PostRepository $postRepository): JsonResponse
     {
         // Crée un nouvel objet Post.
         $post = new Post();
 
+        // Récupère le post dans le body de la requête.
         $request = new Request($_POST);
         $request = $request->toArray();
         $post = $postRepository->find($request['id']);
